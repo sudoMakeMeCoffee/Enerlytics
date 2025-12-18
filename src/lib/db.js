df27dff463df4bae46576dc6b5b2dec6ab8b774a
Achini Subasinghe <achinimaheshika12@gmail.com>
2025-12-18 12:05:18 +0530

    refactor(db): move database configuration to environment variables

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,8 +1,8 @@
 import mysql from 'mysql2/promise';
 
-export const db =await mysql.createPool({
-    host: 'localhost',
-    user:"",
-    password:"",
-    database:"utility_management_system"
-})
+export const db = await mysql.createPool({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME || 'utility_management_system'
+});

44ce8cef97d3c00ef9d5a218706df7520ce3cd7f
AchiniSubasinghe <achinimaheshika12@gmail.com>
2026-01-11 04:05:56 +0000

    automatically redirects to /login

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return (
- <div>
-  
- </div> 
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/login");
+  }, [router]);
+
+  return null;
 }
